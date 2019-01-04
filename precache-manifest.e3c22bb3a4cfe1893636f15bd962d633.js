3f3e01d66708215aa62bfe4b123dd505fb75b80a
TrustBet <eos.trust.bet@gmail.com>
2019-01-04 20:33:30 +0800

    1.init；

---
+++ precache-manifest.e3c22bb3a4cfe1893636f15bd962d633.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cacd86597d3f62fe0e032f1d69277ef3",
+    "url": "/static/media/new_logo.cacd8659.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6cd8b5cb6162026d079a",
+    "url": "/static/js/main.6cd8b5cb.chunk.js"
+  },
+  {
+    "revision": "fefea339947afd3058e2",
+    "url": "/static/js/1.fefea339.chunk.js"
+  },
+  {
+    "revision": "6cd8b5cb6162026d079a",
+    "url": "/static/css/main.072d92a6.chunk.css"
+  },
+  {
+    "revision": "fefea339947afd3058e2",
+    "url": "/static/css/1.e649975d.chunk.css"
+  },
+  {
+    "revision": "ca234c25e2d56a78568cf0c5d140c499",
+    "url": "/index.html"
+  }
+];
